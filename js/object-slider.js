0c966b3cea64a4a5a45afcaefbbe3c4bd383fc86
vbegunova <veronikabegunova42@gmail.com>
2023-12-05 06:54:51 -0600

    Update object-slider.js

--- js/object-slider.js
+++ js/object-slider.js
@@ -1,110 +1,13 @@
-// const objectTitles = document.querySelector(".object-title");
-// const objectDescriptions = document.querySelector(".object-description");
-// const objectPrices = document.querySelector(".object-price");
-// const objectImgs = document.querySelector(".img-obj");
-// const objectSliderPrev = document.querySelector(
-//   ".object-slider-btn-ctrl:first-child"
-// );
-// const objectSliderNext = document.querySelector(
-//   ".object-slider-btn-ctrl:last-child"
-// );
-// const mobileObjectSliderPrev = document.querySelector(
-//   ".mobile-object-slider-btn-ctrl:first-child"
-// );
-// const mobileObjectSliderNext = document.querySelector(
-//   ".mobile-object-slider-btn-ctrl:last-child"
-// );
-// const currentObj = document.querySelector(".current-obj-page");
-// const mobileCurrentObj = document.querySelector(".current-mobile-obj-page");
-
-// let currentIndex = 0;
-// const totalSlides = 5;
-// mobileObjectSliderPrev.disabled = true;
-// objectSliderPrev.disabled = true;
-
-// const titles = [
-//   "Вілла 3 спальні 115 м² в Коста Бланке, Іспанія",
-//   "Апартаменты 45 м² в Calpe, Испания",
-//   "Апартаменти 76 м² в Playa Flamenca, Іспанія",
-//   "Апартаменти, 68 м² у Сан-Мігель-де-Салінас, Іспанія",
-//   "Апартаменти, 65 м² у Campoamor, Іспанія",
-// ];
-
-// const descriptions = [
-//   "Житловий комплекс з двоквартирних та окремих вілл",
-//   "Впечатляющий частный жилой комплекс, расположенный всего в 100 метрах от пляжа",
-//   "Приватний житловий комплекс",
-//   "Сучасний житловий комплекс",
-//   "Приватний житловий комплекс",
-// ];
-
-// const prices = ["399 950", "325 000", "339 000", "154 900", "216 000"];
-
-// const images = [
-//   "./images/actual/actual-costa-blanca.jpg",
-//   "./images/actual/actual-calpe.jpg",
-//   "./images/actual/actual-playa-flamenca.jpg",
-//   "./images/actual/actual-san-miguel-de-salinas.jpg",
-//   "./images/actual/actual-campoamor.jpg",
-// ];
-
-// objectSliderPrev.addEventListener("click", () => {
-//   slideButtonClick("prev");
-// });
-
-// objectSliderNext.addEventListener("click", () => {
-//   slideButtonClick("next");
-// });
-
-// mobileObjectSliderPrev.addEventListener("click", () => {
-//   slideButtonClick("prev");
-// });
-
-// mobileObjectSliderNext.addEventListener("click", () => {
-//   slideButtonClick("next");
-// });
-
-// function slideButtonClick(direction) {
-//   if (direction === "next") {
-//     currentIndex++;
-//     mobileObjectSliderPrev.disabled = false;
-//     objectSliderPrev.disabled = false;
-//     if (currentIndex === totalSlides - 1) {
-//       objectSliderNext.disabled = true;
-//       mobileObjectSliderNext.disabled = true;
-//     }
-//   } else {
-//     currentIndex--;
-//     objectSliderNext.disabled = false;
-//     mobileObjectSliderNext.disabled = false;
-//     if (currentIndex === 0) {
-//       mobileObjectSliderPrev.disabled = true;
-//       objectSliderPrev.disabled = true;
-//     }
-//   }
-
-//   updateSlide(currentIndex);
-// }
-
-// function updateSlide(index) {
-//   objectTitles.innerHTML = titles[index];
-//   objectDescriptions.innerHTML = descriptions[index];
-//   objectPrices.innerHTML = `${prices[index]} €`;
-//   objectImgs.src = images[index];
-//   currentObj.innerHTML = `0${index + 1}`;
-//   mobileCurrentObj.innerHTML = `0${index + 1}`;
-// }
-
-// updateSlide(currentIndex);
-
 const slides = [
   {
+    label: "Новий комплекс на Коста-Бланці, Іспанія",
     title: "Вілла 3 спальні 115 м² в Коста Бланке, Іспанія",
     description: "Житловий комплекс з двоквартирних та окремих вілл",
     price: "399 950",
     image: "./images/actual/actual-costa-blanca.jpg",
   },
   {
+    label: "Новый комплекс в Calpe, Испания",
     title: "Апартаменты 45 м² в Calpe, Испания",
     description:
       "Впечатляющий частный жилой комплекс, расположенный всего в 100 метрах от пляжа",
@@ -112,18 +15,21 @@ const slides = [
     image: "./images/actual/actual-calpe.jpg",
   },
   {
+    label: "Новий комплекс в Playa Flamenca, Іспанія",
     title: "Апартаменти 76 м² в Playa Flamenca, Іспанія",
     description: "Приватний житловий комплекс",
     price: "339 000",
     image: "./images/actual/actual-playa-flamenca.jpg",
   },
   {
+    label: "Новий комплекс у Сан-Мігель-де-Салінас, Іспанія",
     title: "Апартаменти, 68 м² у Сан-Мігель-де-Салінас, Іспанія",
     description: "Сучасний житловий комплекс",
     price: "154 900",
     image: "./images/actual/actual-san-miguel-de-salinas.jpg",
   },
   {
+    label: "Новий комплекс у Campoamor, Іспанія",
     title: "Апартаменти, 65 м² у Campoamor, Іспанія",
     description: "Приватний житловий комплекс",
     price: "216 000",
@@ -131,10 +37,11 @@ const slides = [
   },
 ];
 
-const objectTitles = document.querySelector(".object-title");
-const objectDescriptions = document.querySelector(".object-description");
-const objectPrices = document.querySelector(".object-price");
-const objectImgs = document.querySelector(".img-obj");
+const objectLabel = document.querySelector(".objects-label");
+const objectTitle = document.querySelector(".object-title");
+const objectDescription = document.querySelector(".object-description");
+const objectPrice = document.querySelector(".object-price");
+const objectImg = document.querySelector(".img-obj");
 const objectSliderPrev = document.querySelector(
   ".object-slider-btn-ctrl:first-child"
 );
@@ -157,10 +64,11 @@ objectSliderPrev.disabled = true;
 
 function updateSlide(index) {
   const currentSlide = slides[index];
-  objectTitles.textContent = currentSlide.title;
-  objectDescriptions.textContent = currentSlide.description;
-  objectPrices.textContent = `${currentSlide.price} €`;
-  objectImgs.src = currentSlide.image;
+  objectLabel.textContent = currentSlide.label;
+  objectTitle.textContent = currentSlide.title;
+  objectDescription.textContent = currentSlide.description;
+  objectPrice.textContent = `${currentSlide.price} €`;
+  objectImg.src = currentSlide.image;
   currentObj.textContent = `0${index + 1}`;
   mobileCurrentObj.textContent = `0${index + 1}`;
 }
